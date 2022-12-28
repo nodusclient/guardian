b8403297ca336873b9a9b753dbf203278338d0d5
Ada Cooke <me@ada.rip>
2022-12-28 04:50:29 +0000

    They also said they solved P=NP, Credibility: gone, Sides: in orbit

--- src/main/java/gg/nodus/guardian/mixin/MixinClientPlayNetworkHandler.java
+++ src/main/java/gg/nodus/guardian/mixin/MixinClientPlayNetworkHandler.java
@@ -1,6 +1,8 @@
 package gg.nodus.guardian.mixin;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.message.SignedMessage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -12,8 +14,8 @@ public class MixinClientPlayNetworkHandler {
      * LMFAO
      */
     @ModifyVariable(method = "acknowledge", at = @At("HEAD"), argsOnly = true, index = 2)
-    public boolean acknowledge(boolean value) {
-        return false;
+    public boolean acknowledge(boolean value, SignedMessage message) {
+        return message.getSender().equals(MinecraftClient.getInstance().player.getUuid());
     }
 
 }
